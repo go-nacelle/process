770ac85d14ad278dad7c6179001e7d3398954187
Kristin Davidson <k@kxd.dev>
2022-09-05 22:11:31 -0500

    Add support for setting and retrieving a health tracker with contexts.

---
+++ contexts_test.go
@@ -0,0 +1,36 @@
+package process
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHealthWithAndFromContext(t *testing.T) {
+	t.Run("from context without value", func(t *testing.T) {
+		ctx := context.Background()
+		assert.Nil(t, HealthFromContext(ctx))
+	})
+	t.Run("from context that has value", func(t *testing.T) {
+		h := NewHealth()
+
+		svcKey := "some-service"
+
+		status, err := h.Register(svcKey)
+		require.NoError(t, err)
+		assert.NotNil(t, status)
+		status.Update(true)
+
+		ctx := context.Background()
+		ctx = HealthWithContext(ctx, h)
+
+		ctxHealth := HealthFromContext(ctx)
+		require.NotNil(t, ctxHealth)
+
+		ctxStatus, ok := ctxHealth.Get(svcKey)
+		require.True(t, ok)
+		assert.True(t, ctxStatus.Healthy())
+	})
+}
