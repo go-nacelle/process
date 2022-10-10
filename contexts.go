5f10b58abec50351ff73c8e827481ddee6002b30
Kristin Davidson <k@kxd.dev>
2022-10-10 14:54:23 -0500

    Add support for setting and retrieving a health tracker with contexts. (#18)

---
+++ contexts.go
@@ -0,0 +1,18 @@
+package process
+
+import "context"
+
+type healthKeyType struct{}
+
+var healthKey = healthKeyType{}
+
+func ContextWithHealth(ctx context.Context, health *Health) context.Context {
+	return context.WithValue(ctx, healthKey, health)
+}
+
+func HealthFromContext(ctx context.Context) *Health {
+	if v, ok := ctx.Value(healthKey).(*Health); ok {
+		return v
+	}
+	return nil
+}
